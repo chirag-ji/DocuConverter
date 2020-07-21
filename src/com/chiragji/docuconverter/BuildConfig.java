c89a09f71b3245dab69e7c51bea4cd8c045c16ee
Chirag <apps.chiragji@outlook.in>
2020-07-21 12:56:27 +0530

    Version Update 1.4 code 5

--- src/com/chiragji/docuconverter/BuildConfig.java
+++ src/com/chiragji/docuconverter/BuildConfig.java
@@ -7,10 +7,10 @@ package com.chiragji.docuconverter;
  */
 public interface BuildConfig {
     String APP_NAME = "DocuConverter";
-    String VERSION = "1.3";
-    int BUILD_CODE = 4;
-    String BUILD_DATE = "28-July-2019";
-    String BUILD = VERSION + ".20190728.2243";
+    String VERSION = "1.4";
+    int BUILD_CODE = 5;
+    String BUILD_DATE = "21-July-2020";
+    String BUILD = VERSION + ".20200721.1255";
     String LICENCE = "MIT License";
     String COPYRIGHT_YEAR = "2019";
 
